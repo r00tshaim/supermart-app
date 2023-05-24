3346a187977a624df7303c15e115bb8a795349c9
Shaim Khanusiya <31386526+r00tshaim@users.noreply.github.com>
2023-05-24 09:16:36 +0530

    add navigation for ProductsScreen (#6)

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Tabs from './Tabs';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from '@expo/vector-icons';
 
+import Tabs from "./Tabs";
+
+import ProductsScreen from "../src/screens/ProductsScreen";
 
 const Stack = createStackNavigator();
 
 const MainNavigation = () => {
-    return (
-        <NavigationContainer>
-            <Tabs />
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+
+      <Stack.Navigator>
+        <Stack.Screen
+            name="Tabs"
+            options={{ 
+                headerShown: false 
+            }}
+            component={Tabs}
+        />
+
+        <Stack.Screen
+            name="ProductsScreen"
+            component={ProductsScreen}            
+            options={({ navigation }) => ({
+                //title: 'Products',
+                //headerTitleAlign: 'center',
+                headerLeft: () => (
+                    <Ionicons name="arrow-back-circle" size={24} color="black" onPress={() => navigation.goBack()} />
+                ),
+              })}
+        />
+      </Stack.Navigator>
+      
+    </NavigationContainer>
+  );
 };
 
 export default MainNavigation;
