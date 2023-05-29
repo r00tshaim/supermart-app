a5e62103195d5329646646d23ccf4ec86b7fd619
Shaim Khanusiya <31386526+r00tshaim@users.noreply.github.com>
2023-05-29 14:47:37 +0530

    dispalying orders on order screen and re-structure of order data (#18)

---
+++ src/constants/order.js
@@ -0,0 +1,17 @@
+export const ORDER_STATE = {
+    PROCESSING: "PROCESSING",
+    PLACED: "PLACED",
+    CONFIRMED: "CONFIRMED",
+    OUT_FOR_DELIVERY: "OUT_FOR_DELIVERY",
+    DELIVERED: "DELIVERED",
+    CANCELLED: "CANCELLED",
+}
+
+export const ORDER_STATE_COLOR = {
+    PROCESSING: "#f0ad4e",
+    PLACED: "#0275d8",
+    CONFIRMED: "#5bc0de",
+    OUT_FOR_DELIVERY: "#6194eb",
+    DELIVERED: "#5cb85c",
+    CANCELLED: "#d9534f",
+}
